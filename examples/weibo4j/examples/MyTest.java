5311789cd81c1abd8917ef772fa2d7195a12bbbe
wangyongst <wangyongst@gmail.com>
2019-11-16 10:57:25 +0800

    home

--- examples/weibo4j/examples/MyTest.java
+++ examples/weibo4j/examples/MyTest.java
@@ -1,30 +1,42 @@
 package weibo4j.examples;
 
+import weibo4j.Account;
 import weibo4j.Comments;
 import weibo4j.Timeline;
 import weibo4j.examples.oauth2.Log;
-import weibo4j.model.Comment;
-import weibo4j.model.Status;
-import weibo4j.model.StatusWapper;
-import weibo4j.model.WeiboException;
+import weibo4j.model.*;
 
 import java.util.List;
 
 public class MyTest {
 
     private static String accessToken = "2.007NF7NG2ao8gC39afd3fb4fNgZwhD";
-    private static String commentsText = "不如跟我来学编程啊！http://t.cn/AirY4ViD";
+    private static String commentsText = "不如来跟我学编程啊！http://t.cn/AirY4ViD";
 
     public static void main(String[] args) throws InterruptedException {
         while (true) {
-            StatusWapper statusWapper = getPublicTimeling();
+            RateLimitStatus rateLimitStatus = getAcccountRateLimitStatus();
+            StatusWapper statusWapper = getFriendsTimeline();
             for (Status status : statusWapper.getStatuses()) {
-                Thread.sleep(10000);
-                createComments(commentsText, status.getMid());
+                Thread.sleep(240000);
+                createComments(commentsText, status.getId());
             }
         }
     }
 
+
+    public static RateLimitStatus getAcccountRateLimitStatus() {
+        String access_token = accessToken;
+        Account am = new Account(access_token);
+        try {
+            RateLimitStatus json = am.getAccountRateLimitStatus();
+            Log.logInfo(json.toString());
+            return json;
+        } catch (WeiboException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public static void createComments(String comments, String mid) {
         String access_token = accessToken;
@@ -41,6 +53,18 @@ public class MyTest {
         Timeline tm = new Timeline(accessToken);
         try {
             StatusWapper status = tm.getPublicTimeline();
+            Log.logInfo(status.toString());
+            return status;
+        } catch (WeiboException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static StatusWapper getFriendsTimeline() {
+        Timeline tm = new Timeline(accessToken);
+        try {
+            StatusWapper status = tm.getFriendsTimeline();
             Log.logInfo(status.toString());
             return status;
         } catch (WeiboException e) {
